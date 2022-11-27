8180f55d1fe75d0c1c837152920a977d782b4949
nicteche <nicteche@espol.edu.ec>
2022-11-26 20:06:54 -0500

    subida del examen1

--- app/src/main/java/com/example/amst_e1/GanadorScreem.java
+++ app/src/main/java/com/example/amst_e1/GanadorScreem.java
@@ -8,14 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GanadorScreem extends AppCompatActivity {
     private Button btnSalirG, btnRegresarG;
     private TextView numPreg;
+    private List<String> preguntas, img;
+    private ArrayList<List> respuestas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ganador_screem);
         btnSalirG= (Button) findViewById(R.id.btnSalirG);
+        List<String> preguntas= new List<String>("Cual villano de los increibles?","Que poder tiene Dash?","Cual esa la identidad secreta de Spider-Man","Como se llama el tio de Spiderman?","Cual es el enemigo de Spider-Man?")
+
     }
 
     public void salir(View view){
